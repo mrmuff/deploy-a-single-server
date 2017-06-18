1cf3710cedf20a736f1df8fccbee125c29b3c9a9
mrmuff <mrmuff@icloud.com>
2017-06-18 12:42:27 -0700

    Added .gitignore to repo and update main.tf.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-	region = "us-west-1"
+    region = "us-west-1"
 }
+
 resource "aws_instance" "example" {
-	ami	= "ami-7a85a01a"
-	instance_type = "t2.micro"
-	
-	tags {
-		Name = "terraform-example"
-	}
+    ami = "ami-7a85a01a"
+    instance_type = "t2.micro"
+    tags {
+        Name = "terraform-example"
+    }
 }
